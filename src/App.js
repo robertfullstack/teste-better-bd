560fd0439792f953571d42c4e0d652c5e004efee
robert.silva <robert.silva@dombesni01.besni.com.br>
2026-07-28 16:44:54 -0300

    Teste SQLite local com IndexedDB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,180 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Dexie from "dexie";
+
+
+// cria banco local
+const banco = new Dexie("MeuBancoTeste");
+
+
+// cria tabela
+banco.version(1).stores({
+
+  mensagens:
+    "++id,texto"
+
+});
+
+
 
 function App() {
+
+
+  const [texto, setTexto] = useState("");
+
+  const [lista, setLista] = useState([]);
+
+
+
+
+  useEffect(() => {
+
+    carregar();
+
+  }, []);
+
+
+
+
+
+  async function salvar() {
+
+
+    await banco.mensagens.add({
+
+      texto: texto
+
+    });
+
+
+    setTexto("");
+
+    carregar();
+
+
+  }
+
+
+
+
+  async function carregar() {
+
+
+    const dados =
+      await banco.mensagens
+        .orderBy("id")
+        .reverse()
+        .toArray();
+
+
+    setLista(dados);
+
+
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: 50
+      }}
+    >
+
+
+      <h1>
+        Teste Banco Local
+      </h1>
+
+
+      <input
+
+        value={texto}
+
+        onChange={
+          e => setTexto(e.target.value)
+        }
+
+        placeholder="Digite um texto"
+
+        style={{
+          padding: 10,
+          width: 250
+        }}
+
+      />
+
+
+      <br /><br />
+
+
+      <button
+
+        onClick={salvar}
+
+        style={{
+          padding: 10
+        }}
+
+      >
+
+        Salvar
+
+      </button>
+
+
+
+      <button
+
+        onClick={carregar}
+
+        style={{
+          padding: 10,
+          marginLeft: 10
+        }}
+
+      >
+
+        Buscar
+
+      </button>
+
+
+
+      <hr />
+
+
+      <h2>
+        Dados salvos:
+      </h2>
+
+
+
+      {
+
+        lista.map(item => (
+
+          <p key={item.id}>
+
+            #{item.id} - {item.texto}
+
+          </p>
+
+
+        ))
+
+      }
+
+
+
     </div>
+
   );
+
+
 }
+
 
 export default App;
